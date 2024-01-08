27792b4049383b693776f95079fa074b9fabcf4c
Lithishpichika <147791498+Lithishpichika@users.noreply.github.com>
2024-01-08 12:08:31 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,13 +1,13 @@
 import streamlit as st
 st.set_page_config(page_title='Cats')
-st.header("Types of mangoes")
+st.header("mangoes")
 
 col1, col2 = st.columns(2)
 with col1:
-  st.subheader("mongoes")
+  st.subheader("mongoe")
   st.image("WhatsApp Image 2024-01-06 at 3.28.35 PM.jpeg", caption="mangoes", width=300,use_column_width=True)
-  st.write("mangoes")
+  st.write("mangoe")
 with col2:
-  st.subheader("mango")
+  st.subheader("mangoes")
   st.image("WhatsApp Image 2024-01-06 at 3.28.36 PM.jpeg", caption="mango", width=300,use_column_width=True)
-  st.write("Rawmango")
+  st.write("Rawmangoes")
