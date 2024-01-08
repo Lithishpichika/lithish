da5d4dfe097f94c01393293cda4a5b921aa7c914
Lithishpichika <147791498+Lithishpichika@users.noreply.github.com>
2024-01-08 11:52:37 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -9,5 +9,5 @@ with col1:
   st.write("Persian cats are cute")
 with col2:
   st.subheader("Ragdoll Cat")
-  st.image("./ragdoll.jpg", caption="Ragdoll Cat", width=300,use_column_width=True)
+  st.image("WhatsApp Image 2024-01-06 at 3.28.36 PM.jpeg", caption="Ragdoll Cat", width=300,use_column_width=True)
   st.write("Ragdoll cats are proud")
